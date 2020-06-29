6228e81d8aa1bc42335f0b332ab91c099072da36
Gautier Dervaux <gautier_dervaux@hotmail.com>
2020-06-29 19:04:32 +0200

    cleanup

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 const { ApolloServer } = require("apollo-server");
 const resolvers = require("./src/resolvers").resolvers;
 const typeDefs = require("./src/schema").typeDefs;
-const dao = require("./postgres-node/dao").dao;
 
 const server = new ApolloServer({ typeDefs, resolvers });
-
 server.listen().then(({ url }) => {
   console.log(`🚀  Server ready at ${url}`);
 });
